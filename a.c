5e1b1c49308b15c8d3ce899d60340f45370c8548
hq <1119418168@qq.com>
2021-08-14 21:23:00 +0800

    初版

---
+++ a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int a;
+    a=0;
+    printf("你好                          sadsadadadasdas ");
+    printf("你好                          sadsadadadasdas ");
+    printf("你好                          sadsadadadasdas ");
+    printf("你好                          sadsadadadasdas ");
+    return 0;
+
+}
